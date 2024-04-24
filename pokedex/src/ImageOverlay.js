3093465f11e43aeba4a54e20fb5911ca10392514
Luis-Betancourt93 <betancourtluis619@gmail.com>
2024-04-23 18:25:35 -0700

    Got it to where the pokemon image is showing in the pokedex

--- pokedex/src/ImageOverlay.js
+++ pokedex/src/ImageOverlay.js
@@ -1,23 +1,29 @@
+
 import React from 'react';
 
-const ImageWithTextOverlay = () => {
-  return (
-    
 
-   
+const ImageWithTextOverlay = ({pokemon}) => {
+
+  const image = pokemon ? pokemon.sprites.front_default : '';
+  
+  return (
+
+    
         <div className="relative flex justify-center items-center">
+
           <img
             src="images/pokedex1.png"
             alt="Image the the pokedex"
             className="h-100 ml-52 "
-           
-          />
+           />
 
-        <div className="absolute top-60 -mt-11  h-30  text-white text-lg font-bold bg-green-800  p-2 mr-7 ">
-            Text overlay content
+          {pokemon && (
+          <div className="absolute top-60 -mt-11  h-30 w-40 text-white text-lg font-bold bg-green-800  p-2 mr-7 ">
+           <div className='flex justify-center items-center '><img className='h-20' src={image}/></div>  
           </div>
+         )}
         </div>
-
+      
     
   )
 };
--- pokedex/src/Pokemon.js
+++ pokedex/src/Pokemon.js
@@ -6,13 +6,16 @@ const Pokemon = ({pokemon}) => {
   const image = pokemon.sprites.front_default
   const types = pokemon.types.map(el => el.type.name)
   const typesString = types.join(', ')
+  
   return (
     <div>
       <div>{name}</div>
       <img src={image} alt='Pokemon' />
       <div>{typesString}</div>
     </div>
+    
   )
 }
+
 
 export default Pokemon
