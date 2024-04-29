068ff694e7d1b8b6ce7c8609f623149de0d168b3
Luis-Betancourt93 <betancourtluis619@gmail.com>
2024-04-29 15:10:08 -0700

    Added all the elements color to tailwind and got the background to change with diffrent types of pokemon

--- pokedex/src/ImageOverlay.js
+++ pokedex/src/ImageOverlay.js
@@ -8,9 +8,38 @@ const ImageWithTextOverlay = ({pokemon}) => {
   
   const types = pokemon && pokemon.types ? pokemon.types.map(el => el.type.name) : [];
   const image = pokemon ? pokemon.sprites.front_default : '';
+  let backgroundColorClass = 'bg-black'
+
+  const backgroundColors = {
+    normal: 'bg-normal',
+    fire: 'bg-fire',
+    water: 'bg-water',
+    electric: 'bg-electric',
+    grass: 'bg-grass',
+    ice: 'bg-ice',
+    fighting: 'bg-fighting',
+    poison: 'bg-poison',
+    ground: 'bg-ground',
+    flying: 'bg-flying',
+    psychic: 'bg-psychic',
+    bug: 'bg-bug',
+    rock: 'bg-rock',
+    ghost: 'bg-ghost',
+    dragon: 'bg-dragon',
+    dark: 'steel',
+    fairy: 'bg-fairy'
+
+  }
+  
+  for (const type of types) {
+    if (type in backgroundColors) {
+      backgroundColorClass = backgroundColors[type]
+      break;
+    }
+  }
 
   
-  
+
   return (
 
         // Pokedex and Poke Image
@@ -21,8 +50,8 @@ const ImageWithTextOverlay = ({pokemon}) => {
             className="h-100 ml-52 "
            />
           {pokemon && (
-          <div className="absolute top-60 -mt-11  h-30 w-40 text-white text-lg font-bold bg-green-800  p-2 mr-7 rounded-lg"> 
-           <div className='flex justify-center items-center '><img className='h-36 mb-10 auto ml-2 ' src={image}/></div> 
+          <div className={`${backgroundColorClass} absolute top-60 -mt-11  h-30 w-40 text-white text-lg font-bold   mr-7 rounded-lg`}> 
+           <div className='flex justify-center items-center '><img className='h-34  auto ml-2 ' src={image}/></div> 
           </div>
          )}
 
--- pokedex/tailwind.config.js
+++ pokedex/tailwind.config.js
@@ -5,12 +5,34 @@ module.exports = {
     extend: {
       height: {
         '30': '7.5rem',
+        '34': '8.5rem',
         '38': '9.5rem',
         '100' : '32rem',
       
       },
       padding: {
         '8': '8rem',
+      },
+      colors: {
+        'normal': '#a8a878',
+        'fire': '#F08030',
+        'water': '#6890F0',
+        'electric': '#F8D031',
+        'grass': '#78C850',
+        'ice': '#98D8D8',
+        'fighting': '#C03028',
+        'poison': '#A040A0',
+        'ground': '#E0C068',
+        'flying': '#A890F0',
+        'psychic': '#F85888',
+        'bug': '#A8B820',
+        'rock': '#B8A039',
+        'ghost': '#705898',
+        'dragon': '#7038F8',
+        'dark': '#705848',
+        'steel': '#B8B8D0',
+        'fairy': '#F0B6BC',
+       
       },
     },
   },
