01527e1bb537143f00808d3f0ff0e35521e88c74
Luis-Betancourt93 <betancourtluis619@gmail.com>
2024-04-29 13:55:10 -0700

    fixed the error that it couldnt read properties in types so I made it so it needs to check if its null before accessing its properties

--- pokedex/src/ImageOverlay.js
+++ pokedex/src/ImageOverlay.js
@@ -4,30 +4,30 @@ import React from 'react';
 
 const ImageWithTextOverlay = ({pokemon}) => {
 
+  // Starting to tingger
   
+  const types = pokemon && pokemon.types ? pokemon.types.map(el => el.type.name) : [];
   const image = pokemon ? pokemon.sprites.front_default : '';
 
- 
+  
   
   return (
 
-    
+        // Pokedex and Poke Image
         <div className="relative flex justify-center items-center">
-
           <img
             src="images/pokedex1.png"
             alt="Image the the pokedex"
             className="h-100 ml-52 "
            />
-
           {pokemon && (
-           
-          <div className="absolute top-60 -mt-11  h-30 w-40 text-white text-lg font-bold bg-green-800  p-2 mr-7 rounded-lg">
-           <div className='flex justify-center items-center '><img className='h-36 mb-10 auto ml-2 ' src={image}/></div>  
+          <div className="absolute top-60 -mt-11  h-30 w-40 text-white text-lg font-bold bg-green-800  p-2 mr-7 rounded-lg"> 
+           <div className='flex justify-center items-center '><img className='h-36 mb-10 auto ml-2 ' src={image}/></div> 
           </div>
          )}
 
         </div>
+
       
     
   )
